313018c47d2c1fc2fbf802b376fc78042e08de75
Ianire <ianire.andola@opendeusto.es>
2019-10-28 09:02:25 +0100

    Avanzando con interfaz

--- BetMe/src/Main.java
+++ BetMe/src/Main.java
@@ -1,4 +1,8 @@
 import LD.MySQLAccess;
+import LP.frmAdmin;
+import LP.frmPrincipal;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 public class Main {
 
@@ -13,7 +17,22 @@ public class Main {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		    
+		    try {
+			    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+			        if ("Nimbus".equals(info.getName())) {
+			            UIManager.setLookAndFeel(info.getClassName());
+			            break;
+			        }
+			    }
+			} catch (Exception e) {
+			    // If Nimbus is not available, you can set the GUI to another look and feel.
+			}	
+		    
+		    frmAdmin objPrincipal=new frmAdmin();
+			objPrincipal.setVisible(true);
 		  
 	}
 
 }
+
